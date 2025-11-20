f88710dbbd77b946758c06d71083a4dbcb85b2b0
= <=>
2025-11-20 15:54:46 +0000

    saved results correctly for task 3 started work on measure implementation

--- week3/diagnostic_task.py
+++ week3/diagnostic_task.py
@@ -14,29 +14,35 @@ See submission_format.txt for details.
 from transformers import pipeline
 import time
 
+def Repitionscore(text):
+    # find % along text till repition
+    return 0
+
 # LEVEL 1: Basic generation
 print("=== LEVEL 1: BASIC GENERATION ===")
+models = ['distilgpt2', 'gpt2', 'gpt2-medium']
 generators = [pipeline('text-generation', model='distilgpt2'),pipeline('text-generation', model='gpt2'),pipeline('text-generation', model='gpt2-medium')]
 
 prompts = [
-    "apple apple apple"
+    "apple apple"
 ]
 
 filesavedata = {}
 for prompt in prompts:
     filesavedata[prompt] = {}
 
-for generator in generators:
+for m in models:
     for prompt in prompts:
+        generator = pipeline('text-generation', model = m)
         starttime = time.time()
         output = generator(prompt, max_length=30, temperature = 0.3, top_k = 50)
         print(f"\nPrompt: {prompt}")
         print(f"Generated: {output[0]['generated_text'].strip()}")
         print("-" * 50)
         tokens = len(generator.tokenizer.encode(output[0]['generated_text']))
-        filesavedata[prompt][generator] =  (output[0]['generated_text'], time.time() - starttime, tokens)
+        filesavedata[prompt][m] =  (output[0]['generated_text'], time.time() - starttime, tokens)
 
-with open(".\\week3\\results.txt", "w") as file:
+with open("results.txt", "w") as file:
     for k,v in filesavedata.items():
         for model, data in v.items():
             file.write(f"Prompt:{k}\nGenerated with {model}:{data[0].strip()}\nTime to generate: {data[1]}\nNumber of tokens{data[2]}\n\n\n")
